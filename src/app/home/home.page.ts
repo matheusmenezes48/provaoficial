235f3432904409a7232819458f18a85986fd9dcc
Your Name <you@example.com>
2019-07-17 18:53:42 -0300

    tela de inicial com o nome e foto do usuário

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  usuario = {
+    "nome":"Matheus",
+    "imagem":"https://f.i.uol.com.br/fotografia/2019/03/15/15526795065c8c025270c53_1552679506_4x3_sm.jpg"
+  }
 
   constructor() {}
 
