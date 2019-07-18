03adab72edefd17ca8a55269d1b5b34818f4d9ad
Your Name <you@example.com>
2019-07-17 21:19:38 -0300

    implementei função da camera

--- src/app/usuario-modal/usuario-modal.page.ts
+++ src/app/usuario-modal/usuario-modal.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx'; 
+
 
 
 @Component({
@@ -12,12 +14,25 @@ export class UsuarioModalPage implements OnInit {
     "nome":"",
     "imagem":""
   }
-  constructor(public modalcontroler: ModalController) { }
+  constructor(public modalcontroler: ModalController, private camera: Camera) { }
   atualizar(){
     this.modalcontroler.dismiss(this.usuario)
   }
 
   ngOnInit() {
   }
+  foto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+    }
 
+    this.camera.getPicture(options).then((imageData) => {
+      this.usuario.imagem = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      alert(err)
+    });
+  }
 }
